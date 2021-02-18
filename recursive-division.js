a5a4e13a4309f6e8f11b03aa4b919e219773e8f9
Nick On <nick.on437@gmail.com>
2021-02-18 12:01:10 +1030

    ✨ New - Prevent new wall to block existing door

--- recursive-division.js
+++ recursive-division.js
@@ -2,6 +2,12 @@ import { app, delay } from './script.js';
 
 const rand = (max) => Math.floor(Math.random() * max + 1);
 const randInRange = (min, max) => rand(max - min) + min - 1;
+
+const ORIENTATION = {
+  VERTICAL: 'vertical',
+  HORIZONTAL: 'horizontal',
+};
+
 let num = 0;
 // TODO: Set minimum row and col.. maybe 7 ???
 const buildRecursiveMaze = async () => {
@@ -24,24 +30,104 @@ const buildRecursiveMaze = async () => {
   buildWall(1, app.numCol - 2, 1, app.numRow - 2, true);
 };
 
+const getOrientation = (width, height) => {
+  if (width < height) {
+    return ORIENTATION.HORIZONTAL;
+  } else if (height < width) {
+    return ORIENTATION.VERTICAL;
+  } else {
+    return rand(2) === 1 ? ORIENTATION.HORIZONTAL : ORIENTATION.VERTICAL;
+  }
+};
+
+const isConnectedWithWall = (
+  wallIndex,
+  headIndex,
+  tailIndex,
+  isVerticalWall
+) => {
+  if (isVerticalWall) {
+    if (
+      app.boardArr[headIndex - 1][wallIndex].isWall &&
+      app.boardArr[tailIndex + 1][wallIndex].isWall
+    ) {
+      return true;
+    }
+  } else {
+    if (
+      app.boardArr[wallIndex][headIndex - 1].isWall &&
+      app.boardArr[wallIndex][tailIndex + 1].isWall
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const generateWallIndex = (
+  lowerBoundIndex,
+  upperBoundIndex,
+  headIndex,
+  tailIndex,
+  isVerticalWall
+) => {
+  let hasPotentialWall = false;
+  for (let i = lowerBoundIndex; i < upperBoundIndex; i++) {
+    hasPotentialWall = isConnectedWithWall(
+      i,
+      headIndex,
+      tailIndex,
+      isVerticalWall
+    );
+    if (hasPotentialWall) {
+      break;
+    }
+  }
+
+  if (!hasPotentialWall) {
+    return -1;
+  }
+
+  let wallIndex = randInRange(lowerBoundIndex, upperBoundIndex);
+  while (
+    !isConnectedWithWall(wallIndex, headIndex, tailIndex, isVerticalWall)
+  ) {
+    wallIndex = randInRange(lowerBoundIndex, upperBoundIndex);
+  }
+
+  return wallIndex;
+};
+
 const buildWall = async (
   lowerBoundIndex,
   upperBoundIndex,
   headIndex,
   tailIndex,
-  isVerticalWall,
+  isVerticalWall
 ) => {
   if (upperBoundIndex - lowerBoundIndex - 2 < 1) {
     return false;
   }
+
+  if (num >= 50) return;
   // if (bottomIndex - topIndex - 2 < 1) {
   //   return false;
   // }
 
-  let wallIndex = randInRange(lowerBoundIndex + 1, upperBoundIndex - 1);
+  let wallIndex = generateWallIndex(
+    lowerBoundIndex + 1,
+    upperBoundIndex - 1,
+    headIndex,
+    tailIndex,
+    isVerticalWall
+  );
   const doorIndex = randInRange(headIndex, tailIndex);
 
-  // num++;
+  if (wallIndex === -1) {
+    return false;
+  }
+
+  num++;
   // console.log('left right top bottom', lowerBoundIndex, upperBoundIndex, headIndex, tailIndex, 'wallIndex', wallIndex, 'doorIndex', doorIndex);
   for (let i = headIndex; i <= tailIndex; i++) {
     if (
@@ -55,13 +141,28 @@ const buildWall = async (
     }
 
     await delay(100);
-    const cell = isVerticalWall ? app.boardArr[i][wallIndex] : app.boardArr[wallIndex][i];
+    const cell = isVerticalWall
+      ? app.boardArr[i][wallIndex]
+      : app.boardArr[wallIndex][i];
     cell.isWall = true;
     cell.classList.add('wall');
   }
 
-  await buildWall(headIndex, tailIndex, lowerBoundIndex, wallIndex - 1, !isVerticalWall);
-  await buildWall(headIndex, tailIndex, wallIndex + 1, upperBoundIndex, !isVerticalWall);
+  await buildWall(
+    headIndex,
+    tailIndex,
+    lowerBoundIndex,
+    wallIndex - 1,
+    !isVerticalWall
+  );
+
+  await buildWall(
+    headIndex,
+    tailIndex,
+    wallIndex + 1,
+    upperBoundIndex,
+    !isVerticalWall
+  );
 
   return true;
 };
