e29d4a8d0c0927468bb0d897660aa45e48b3edb5
Nick On <nick.on437@gmail.com>
2021-02-20 22:02:11 +1030

    🎨 Style - Restyle nav

--- script.js
+++ script.js
@@ -12,50 +12,54 @@ import mgDfs from './maze-generation/mgDfs.js';
 
 init();
 
-$('#start-btn').on('click', () => {
-  if (app.start && app.target) {
-    startPathFinding();
-  }
-});
-
-$('#clear-path-btn').on('click', () => {
-  clearSearchResult();
-
-  app.isFirstRun = true;
-  app.board.removeClass('no-animation');
-  clearTimeout(app.removeAnimationTimeout); // For edge case, when board is cleared during board is waiting for no-animation to be added
-});
-
-$('#clear-walls-btn').on('click', () => {
-  clearSearchResult();
-  clearWalls();
-
-  app.isFirstRun = true;
-  app.board.removeClass('no-animation');
-  clearTimeout(app.removeAnimationTimeout); // For edge case, when board is cleared during board is waiting for no-animation to be added
-});
-
-$('#path-finding-algorithm').on('change', () => {
-  rerenderPath();
+$('.dropdown-toggle').on('click', (e) => {
+  e.target.parentNode.classList.toggle('opened');
 })
 
-$('#maze-generation-algorithm').on('change', async (e) => {
-  clearSearchResult();
-  clearWalls();
+// $('#start-btn').on('click', () => {
+//   if (app.start && app.target) {
+//     startPathFinding();
+//   }
+// });
 
-  switch (e.target.value) {
-    case 'random':
-      buildBasicRandMaze();
-      break;
-    case 'recursive-division':
-      await buildRecursiveMaze();
-      break;
-    case 'dfs':
-      await mgDfs();
-      break;
-    default:
-      break;
-  }
+// $('#clear-path-btn').on('click', () => {
+//   clearSearchResult();
 
-  rerenderPath();
-});
+//   app.isFirstRun = true;
+//   app.board.removeClass('no-animation');
+//   clearTimeout(app.removeAnimationTimeout); // For edge case, when board is cleared during board is waiting for no-animation to be added
+// });
+
+// $('#clear-walls-btn').on('click', () => {
+//   clearSearchResult();
+//   clearWalls();
+
+//   app.isFirstRun = true;
+//   app.board.removeClass('no-animation');
+//   clearTimeout(app.removeAnimationTimeout); // For edge case, when board is cleared during board is waiting for no-animation to be added
+// });
+
+// $('#path-finding-algorithm').on('change', () => {
+//   rerenderPath();
+// })
+
+// $('#maze-generation-algorithm').on('change', async (e) => {
+//   clearSearchResult();
+//   clearWalls();
+
+//   switch (e.target.value) {
+//     case 'random':
+//       buildBasicRandMaze();
+//       break;
+//     case 'recursive-division':
+//       await buildRecursiveMaze();
+//       break;
+//     case 'dfs':
+//       await mgDfs();
+//       break;
+//     default:
+//       break;
+//   }
+
+//   rerenderPath();
+// });
